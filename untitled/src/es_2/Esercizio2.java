3b0d45a6aaeace5f8b94c5e4ab3a03c7c1498ac9
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-09-10 16:56:26 +0200

    comment

--- untitled/src/es_2/Esercizio2.java
+++ untitled/src/es_2/Esercizio2.java
@@ -7,7 +7,7 @@ public class Esercizio2 {
         System.out.println("Creazione di una nuova SIM:");
         simUtente.stampaDatiSim();
 
-        // Simuliamo alcune chiamate per dimostrare il funzionamento
+        // Creo chiamate
         simUtente.aggiungiChiamata(new Chiamata(5, "+393456789012"));
         simUtente.aggiungiChiamata(new Chiamata(2, "+393210987654"));
         simUtente.aggiungiChiamata(new Chiamata(12, "+393451122334"));
@@ -15,7 +15,7 @@ public class Esercizio2 {
         System.out.println("\nDopo aver effettuato 3 chiamate:");
         simUtente.stampaDatiSim();
 
-        // Aggiungiamo altre chiamate per mostrare il limite di 5
+        // Aggiungo altre chiamate per mostrare il limite di 5
         simUtente.aggiungiChiamata(new Chiamata(3, "+393339876543"));
         simUtente.aggiungiChiamata(new Chiamata(8, "+393481234567"));
         simUtente.aggiungiChiamata(new Chiamata(1, "+393290001112"));
